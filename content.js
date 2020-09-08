f443aa20f877e174dd8ca2bddc3161c6947fc07a
Ash West <ashlandmwest@gmail.com>
2020-09-08 00:19:42 -0700

    Add procedures variable and reformat with prettier

--- content.js
+++ content.js
@@ -1,75 +1,86 @@
 const currentURL = window.location.href;
 const rootDomain = window.location.hostname;
 
-const topLevelNav = document.getElementById('nav');
+const topLevelNav = document.getElementById("nav");
 let navItems = [];
 if (topLevelNav) {
-  Array.from(topLevelNav.children).forEach(navElement => {
-    navItem = '';
-    navItem = navElement.querySelector('a').getAttribute('data-searchable-tag')
+  Array.from(topLevelNav.children).forEach((navElement) => {
+    navItem = "";
+    navItem = navElement.querySelector("a").getAttribute("data-searchable-tag");
     navItems.push(navItem);
-  })
+  });
 }
 
-let procedureLists = navItems.filter(page =>
-  page.includes('Procedures') || page.includes('Services')
-)
+let procedureLists = navItems.filter(
+  (page) => page.includes("Procedures") || page.includes("Services")
+);
+
+let exclusionList = ["3D Imaging", "Stem Cells", "Stem-Cells", "Stemcells"];
+
+let procedures = [];
 
 isInDevelopment = () => {
-  if (rootDomain === 'www.freewaysites.com') return true
-  if (rootDomain === 'www.pbhssites.com') return true
+  if (rootDomain === "www.freewaysites.com") return true;
+  if (rootDomain === "www.pbhssites.com") return true;
   return false;
-}
+};
 
 developmentFilter = (domain) => {
-  pathArray = window.location.pathname.split('/')
-  return `${domain}/${pathArray[1]}`
-}
+  pathArray = window.location.pathname.split("/");
+  return `${domain}/${pathArray[1]}`;
+};
 
-const workingDomain = isInDevelopment() ? developmentFilter(rootDomain) : window.location.hostname;
-
+const workingDomain = isInDevelopment()
+  ? developmentFilter(rootDomain)
+  : window.location.hostname;
 
 chrome.runtime.onMessage.addListener(function (request) {
-  if (request === 'hi') {
+  if (request === "hi") {
     alert(request);
   }
-})
+});
 
 chrome.runtime.onMessage.addListener(function (request) {
-  if (request === 'queryData') {
+  if (request === "queryData") {
     for (list of procedureLists) {
       try {
         if (document.querySelector('[itemprop="copyrightHolder"]')) {
-          if (document.querySelector('[itemprop="copyrightHolder"]').innerHTML === 'PBHS') {
-            if (document.querySelector('[data-searchable-tag=' + CSS.escape(list) + ']')) {
-              let childElements = document.querySelector('[data-searchable-tag=' + CSS.escape(list) + ']').parentElement.children;
+          if (
+            document.querySelector('[itemprop="copyrightHolder"]').innerHTML ===
+            "PBHS"
+          ) {
+            if (
+              document.querySelector(
+                "[data-searchable-tag=" + CSS.escape(list) + "]"
+              )
+            ) {
+              let childElements = document.querySelector(
+                "[data-searchable-tag=" + CSS.escape(list) + "]"
+              ).parentElement.children;
               let childList = {};
               for (element in Array.from(childElements)) {
-                if (childElements[element].className === 'children') {
+                if (childElements[element].className === "children") {
                   childList = childElements[element];
                   break;
                 }
               }
               for (child in Array.from(childList.children)) {
-                console.log(childList.children[child].querySelector('a').innerText)
+                console.log(
+                  childList.children[child].querySelector("a").innerText
+                );
               }
             } else {
-              throw `Can't find "${workingDomain}/${list}"`
+              throw `Can't find "${workingDomain}/${list}"`;
             }
           } else {
-            throw 'Not a PBHS Site'
+            throw "Not a PBHS Site";
           }
         } else {
-          throw 'Not a PBHS Site'
+          throw "Not a PBHS Site";
         }
-      }
-      catch (err) {
+      } catch (err) {
         if (err) alert(err);
       }
     }
   }
-})
-
-
-
-
+});
